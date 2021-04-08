927f12d678af1b8afeadb8577607af1fa2fea332
Morghan <morghan102@gmail.com>
2021-04-08 09:15:02 -0700

    Added components

---
+++ shared/comments.js
@@ -0,0 +1,163 @@
+export const COMMENTS = 
+[
+    {
+        id: 0,
+        serviceId: 0,
+        rating: 5, 
+        text: "Something here.",
+        author: "Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        serviceId: 0,
+        rating: 4, 
+        text: "This.",
+        author: "Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        serviceId: 0,
+        rating: 5, 
+        text: "Is.",
+        author: "McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        serviceId: 0,
+        rating: 5, 
+        text: "Probably.",
+        author: "Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        serviceId: 0,
+        rating: 4, 
+        text: "not",
+        author: "Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 5,
+        serviceId: 1,
+        rating: 4, 
+        text: "worth.",
+        author: "Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 6,
+        serviceId: 1,
+        rating: 5, 
+        text: "It",
+        author: "McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 7,
+        serviceId: 1,
+        rating: 5, 
+        text: "to",
+        author: "Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 8,
+        serviceId: 1,
+        rating: 4, 
+        text: "include.",
+        author: "Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 9,
+        serviceId: 1,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 10,
+        serviceId: 2,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 11,
+        serviceId: 2,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 12,
+        serviceId: 2,
+        rating: 4, 
+        text: "The stars at night were a revelation!",
+        author: "Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 13,
+        serviceId: 2,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 14,
+        serviceId: 2,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 15,
+        serviceId: 3,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 16,
+        serviceId: 3,
+        rating: 4, 
+        text: "The stars at night were a revelation!",
+        author: "Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 17,
+        serviceId: 3,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 18,
+        serviceId: 3,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 19,
+        serviceId: 3,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    }
+]
---
+++ shared/services.js
@@ -0,0 +1,34 @@
+export const SERVICES = [
+    {
+        id: 0,
+        name: "Pet Sitting",
+        image: "/assets/images/arabica.jpg",
+        price: 50,
+        featured: false,
+        description: "Something here."
+    },
+    {
+        id: 1,
+        name:"Day Care",
+        image: "/assets/images/arabica.jpg",
+        price: 40,
+        featured: false,
+        description: "Something here."
+    },
+    {
+        id: 2,
+        name:"Grooming",
+        image: "/assets/images/arabica.jpg",
+        price: 10,
+        featured: false,
+        description: "Something here."
+    },
+    {
+        id: 3,
+        name:"Walking",
+        image: "/assets/images/arabica.jpg",
+        price: 42,
+        featured: true,
+        description: "Something here."
+    }
+];
