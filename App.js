6acc3a198a477171fd014f3abe5af3a121526e2b
Morghan <morghan102@gmail.com>
2021-07-16 14:38:53 -0700

    initial addition to app for chat with firebase

--- App.js
+++ App.js
@@ -3,6 +3,20 @@ import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 const store = ConfigureStore();
 
 export default function App() {
