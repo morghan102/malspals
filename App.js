2dee6b9508f98bddea985892ca2498ed3357adfb
Morghan <morghan102@gmail.com>
2021-07-20 11:03:20 -0700

    adding firebase connection

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
+import * as firebase from 'firebase'; // i may want to remove the other imports 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -10,9 +11,19 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
+// Initialize Firebase. I cannot find these values
+const firebaseConfig = {
+    apiKey: "AIzaSyDAPKZZPwH4byxTWnHMawwCnsbufZvpuKk",
+    authDomain: "<your-auth-domain>",
+    databaseURL: "<your-database-url>",
+    storageBucket: "<your-storage-bucket>",
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-})
+//this is from the react app for making chat comp
+// firebase.initializeApp({
+
+// })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
